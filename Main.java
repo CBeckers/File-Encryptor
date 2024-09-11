b8070bfcd9fc66cf99fb7aa6550eb3b773024f27
CBeckers <149545700+CBeckers@users.noreply.github.com>
2024-09-11 15:06:27 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,10 +7,10 @@ import java.io.IOException;
  * @author Cade Beckers | 7/25/2024
  * Revised: 9/11/2024
  */
-public class Project {
+public class Main {
 
     public static void main(String[] args) throws IOException {
-        MyEncryption encrypter = new MyEncryption();
-        encrypter.main();
+        Encryption encryptor = new Encryption();
+        encryptor.main();
     }
 }
